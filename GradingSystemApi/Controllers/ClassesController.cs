945b71a2b2db4d593d3f36d5f864e2acb91a25de
Ezik143 <jakesucgang90@gmail.com>
2025-05-03 18:55:58 +0800

    test this for bugs

---
+++ GradingSystemApi/Controllers/ClassesController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Team_Yeri_enrollment_system.GradingLibrary.Data;
+using Team_Yeri_enrollment_system.GradingLibrary.Models;
+
+namespace GradingSystemApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassesController : ControllerBase
+    {
+        private readonly EnrollmentDbContext _context;
+
+        public ClassesController(EnrollmentDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/classes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Classes>>> GetClasses()
+        {
+            return await _context.Classes.ToListAsync();
+        }
+
+        // GET: api/classes/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Classes>> GetClass(int id)
+        {
+            var classItem = await _context.Classes.FindAsync(id);
+            if (classItem == null)
+            {
+                return NotFound();
+            }
+            return classItem;
+        }
+
+        // POST: api/classes
+        [HttpPost]
+        public async Task<ActionResult<Classes>> CreateClass(Classes newClass)
+        {
+            _context.Classes.Add(newClass);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetClass), new { id = newClass.Class_ID }, newClass);
+        }
+
+        // PUT: api/classes/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateClass(int id, Classes updatedClass)
+        {
+            if (id != updatedClass.Class_ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(updatedClass).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClassExists(id))
+                {
+                    return NotFound();
+                }
+                throw;
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/classes/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClass(int id)
+        {
+            var classItem = await _context.Classes.FindAsync(id);
+            if (classItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.Classes.Remove(classItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ClassExists(int id)
+        {
+            return _context.Classes.Any(e => e.Class_ID == id);
+        }
+    }
+}
--- GradingSystemApi/Program.cs
+++ GradingSystemApi/Program.cs
@@ -1,13 +1,24 @@
 using Team_Yeri_enrollment_system.GradingLibrary.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Grading System API",
+        Version = "v1"
+    });
+});
+
 
 builder.Services.AddDbContext<EnrollmentDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("GradingSystemApiConnectionString")));
@@ -17,13 +28,18 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Grading System API V1");
+        c.RoutePrefix = string.Empty; // Set Swagger UI at the app's root
+    });
 }
 
+
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
+
 
 app.Run();
